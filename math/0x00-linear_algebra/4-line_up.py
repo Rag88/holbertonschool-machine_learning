ce8da6a82b86be03764112d04fbd2d96f36950de
Rag88 <4555@holbertonstudents.com>
2022-12-03 02:13:43 +0000

    Line up

---
+++ math/0x00-linear_algebra/4-line_up.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" function to add two arrays elements-wise """
+
+
+def add_arrays(arr1, arr2):
+    """ Ading two arrays elements-wise
+
+    Args: arr1, arr2: Given arrays
+
+    Return: the sum of arrays (new matrix)
+
+    """
+    if len(arr1) != len(arr2):
+        return None
+    else:
+        return [sum(x) for x in zip(arr1, arr2)]
